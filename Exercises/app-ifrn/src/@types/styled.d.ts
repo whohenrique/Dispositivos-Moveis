9aa5f33e95c89c0adb064e609c381715242fc02e
Henrique <heduardocr7@gmail.com>
2024-08-08 22:17:57 -0300

    delete app-ifrn

--- Exercises/app-ifrn/src/@types/styled.d.ts
+++
@@ -1,7 +0,0 @@
-import "styled-components";
-import { ThemeType } from "../global/styles/theme";
-
-declare module "styled-components" {
-    type ThemeType = typeof theme;
-    export interface DefaultTheme extends ThemeType {}
-}
--- Exercises/app-ifrn/src/components/count-task/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components/native";
-
-export const Count = styled.Text<{ count: boolean }>`
-  font-size: 16px;
-  color: #fff;
-
-  ${({ count }) => count && `font-weight: bold;`};
-`;
